31bccaddf8b59aadf3c8850e540cb5357b88add9
AnoniemeBlobvis <92373002+AnoniemeBlobvis@users.noreply.github.com>
2022-04-16 11:30:50 +0200

    Added API for getting crypto rates

--- views/exchange.php
+++ views/exchange.php
@@ -1,5 +1,22 @@
 <?php
-
 echo "exchange";
 
+// set API Access Key
+$access_key = 'c194de628d8f680f6c98df475e1edaa0';
+
+// Initialize CURL:
+$ch = curl_init('http://api.coinlayer.com/live?access_key=' . $access_key . '&target=EUR');
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Store the data:
+$json = curl_exec($ch);
+curl_close($ch);
+
+// Decode JSON response:
+$arr_result = json_decode($json, true);
+
+$BTCPrice = $arr_result['rates']["BTC"];
+
+print($BTCPrice);
 ?>
