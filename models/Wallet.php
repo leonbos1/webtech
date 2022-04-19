f8aa3dc858244251eba3bd864062de5a01b84096
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2022-04-19 22:03:45 +0200

    euro toevoeg functie
    + wat psuedo code preparatie om geld te converten

--- models/Wallet.php
+++ models/Wallet.php
@@ -2,10 +2,19 @@
 
 namespace app\models;
 
+use app\core\Application;
 use app\core\DatabaseModel;
 
 class Wallet extends DatabaseModel
 {
+    public int $user_id;
+    public int $euro;
+    public int $BTC;
+    public int $ETH;
+    public int $LTC;
+    public int $XRP;
+    public int $DOGE;
+
     public static function tableName(): string
     {
         return "wallet";
@@ -18,6 +27,26 @@ class Wallet extends DatabaseModel
 
     public static function getWalletByUser($user) {
         return Wallet::findOne(["user_id" => $user->id]);
+    }
+
+    public function addEuros($amount) {
+        $tablename = self::tableName();
+        $new_amount = $this->euro + $amount;
+        $statement = Application::$app->database->connection->prepare("update $tablename set euro = $new_amount where user_id = $this->user_id");
+
+        $statement->execute();
+    }
+
+    public function convertCurrency($beforeConvert, $afterConvert, $amount) {
+
+        // $newAmount = ($amount * omgerekende waarde)
+        // oldAmountBeforeConvert = totale beforeConvert
+        // newAmountBeforeConvert = oldAmountBeforeConvert - $amount
+        // update in database beforeConvert = beforeConvert = newAmountBeforeConvert
+        // oldAmountAfterConvert = totale afterConvert
+        // newAmountAfterConvert = oldAmountAfterConvert + $newAmount
+        // update in database afterConvert = afterConvert = newAmountAfterConvert
+
     }
 
 }
